a16db70cc3803c1b608f6d954d63860a4c9d1641
YannGoulart <yzyann7@gmail.com>
2026-08-27 20:54:00 -0300

    Declarado o método Robo::calcular_distancia e Robo::determinar_robo_mais_proximo, não houve erro de compilação durante a declaração.

--- Robo.cpp
+++ Robo.cpp
@@ -19,22 +19,45 @@ void Robo::mover(double v, double th, double t){
     double distancia_vertical = distancia_percorrida * std::sin(th);
     double distancia_horizontal = distancia_percorrida * std::cos(th);
 
-    // Alterando a nova posiçaõ no próprio objeto
+    // Alterando a nova posição no próprio objeto
     this->_posicao._x += distancia_horizontal;
     this->_posicao._y += distancia_vertical;
 }
 
-    // método quecalcula e retorna a distancia euclidiana para outro robo passado como paramettro  
-    double calcular_distancia(Robo* robo);
+// método que calcula e retorna a distancia euclidiana para outro robo passado como parametro
+double Robo::calcular_distancia(Robo* robo){
+    // Utiliza o método de Ponto2D pra calcular essa distância
+    return this->_posicao.calcular_distancia(&robo->_posicao);
+}
 
-    // metodo que recebe um array de ponteiros (o time) e um parametro que informa quantos elementos no array, retorna o robo mais próximo do que invocou o método e retorna um ponteiro para ele
-    Robo* determinar_robo_mais_proximo(Robo** naves, int n);
+// metodo que recebe um array de ponteiros (o time) e um parametro que informa quantos elementos no array, retorna o robo mais próximo do que invocou o método e retorna um ponteiro para ele
+Robo* Robo::determinar_robo_mais_proximo(Robo** naves, int n){
+    Robo* mais_proximo = this;
+    double distancia_mais_proxima = 0;
+    double distancia_ate_robo = 0;
 
-    // recebe um array de ponteiros para robos (o time) e um parametro que informa a quantidade de elementos desse array.
-    // Passar a bola significa apenas modificar os valores do atributo '_com_bola'
-    // Se o robô que invocou o método não estiver com a bola, imprime: "estou sem bola" com quebra de linha
-    void passar_bola(Robo** time, int n);
+    for (int i = 0; i < n; i ++)
+    {
+        distancia_ate_robo = this->calcular_distancia(naves[i]);
 
-    // Imprime o estatus atual do robo
-    //id x y com_bola energia, utilizando tab(\t) \t para separar
-    void imprimir_status();
+        if (i == 0){
+            distancia_mais_proxima = distancia_ate_robo;
+            mais_proximo = naves[i];
+        }
+        if (distancia_ate_robo < distancia_mais_proxima){
+            distancia_mais_proxima = distancia_ate_robo;
+            mais_proximo = naves[i];
+        }
+    }
+
+    return mais_proximo;
+}
+
+// recebe um array de ponteiros para robos (o time) e um parametro que informa a quantidade de elementos desse array.
+// Passar a bola significa apenas modificar os valores do atributo '_com_bola'
+// Se o robô que invocou o método não estiver com a bola, imprime: "estou sem bola" com quebra de linha
+void passar_bola(Robo** time, int n);
+
+// Imprime o estatus atual do robo
+//id x y com_bola energia, utilizando tab(\t) \t para separar
+void imprimir_status();
